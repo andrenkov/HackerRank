1700cb823cf9c15a9f0fefd8e4006fb516f23169
andrenkov <andrenkov@gmail.com>
2022-09-20 10:35:09 -0400

    test 01 and 02 added

---
+++ HackerRank30Days/2022Test01/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2022Test01
+{
+    class Result
+    {
+
+        /*
+         * Complete the 'miniMaxSum' function below.
+         *
+         * The function accepts INTEGER_ARRAY arr as parameter.
+         */
+
+        public static void miniMaxSum(List<int> arr)
+        {
+            long maxNum = arr[0];
+            long minNum = arr[0];
+            long Sum = 0;
+
+            foreach (var item in arr)
+            {
+                Sum += item;
+                if (item < minNum)
+                    minNum = item;
+                if (item > maxNum)
+                    maxNum = item;
+            }
+            Console.WriteLine(string.Format("{0} {1}", Sum - maxNum, Sum - minNum));
+        }
+
+    }
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.miniMaxSum(arr);
+
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();
+        }
+    }
+
+}
---
+++ HackerRank30Days/2022Test02/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthdayCakeCandles' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY candles as parameter.
+     */
+
+    public static int birthdayCakeCandles(List<int> candles)
+    {
+        int res = 0;
+        int tolest = candles[0];
+
+        foreach (var item in candles)
+        {
+            if (item == tolest)
+            {
+                res++;
+            }
+            else
+            {
+                if (item > tolest)
+                {
+                    tolest = item;
+                    res = 1;
+                }
+            }
+        }
+        return res;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(Environment.CurrentDirectory, true);
+        //TextWriter textWriter = new StreamWriter(Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory), true);
+
+        int candlesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> candles = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(candlesTemp => Convert.ToInt32(candlesTemp)).ToList();
+
+        int result = Result.birthdayCakeCandles(candles);
+        Console.WriteLine(result);
+
+        //textWriter.WriteLine(result);
+
+        //textWriter.Flush();
+        //textWriter.Close();
+
+        Console.Write("\nPress any key to continue... ");
+        Console.ReadLine();
+    }
+}
+
